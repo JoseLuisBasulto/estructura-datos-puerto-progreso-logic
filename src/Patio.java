ecb8a9cb723bf0220194328fe923fb603aa2b46a
Francois Pageau <francoispageaus@gmail.com>
2026-02-11 10:29:53 -0600

    comentarios del modulo B agregados

--- src/Patio.java
+++ src/Patio.java
@@ -8,6 +8,8 @@ public class Patio {
 
     public Patio() { almacén = new ListaSimple();}
 
+    // Se agrega una nueva columna al almacén por medio de una inserción de una nueva pila a la lista de pilas
+    // La función no recibe argumentos ni devuelve la referencia de algún objeto
     public void agregarPila() {
         Scanner sc = new Scanner(System.in);
 
@@ -16,7 +18,9 @@ public class Patio {
         almacén.insertaFinal(new PilaContenedores(nombre));
     }
 
-    public PilaContenedores seleccionarPila() { // El usuario eligirá qué pila quiere manipular
+    // Para facilitar la modificación de una columna, esta función le permite al usuario escoger una pila en específico
+    // La función no recibe ningún parámetro, pero devuelve la referencia de la pila elegida por el usuario
+    public PilaContenedores seleccionarPila() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Pilas registradas:");
@@ -36,7 +40,10 @@ public class Patio {
 
     }
 
-    public void agregarContenedor(PilaContenedores pilaActual, Contenedor contenedor) { // Agregar contenedor a una pila ya elegida
+    // Se agrega un contenedor a la pila seleccionada anteriormente, este contenedor ya debió pasar por recepción (módulo A)
+    // La función recibe el contenedor elegido y la pila específica que se está modificando. No devuelve nada la función
+    // TODO: Modificar concorde a lo platicado en el scrum
+    public void agregarContenedor(PilaContenedores pilaActual, Contenedor contenedor) {
         if (!validarPila(pilaActual)) {
             System.out.println("Esta pila de contenedores llegó a su límite. Registra una nueva pila.");
         } else {
@@ -44,19 +51,28 @@ public class Patio {
         }
     }
 
-    public void retirarContenedor(PilaContenedores pilaActual) { // Eliminar contenedor de una pila ya elegida
-        if (!pilaActual.isEmpty()) {
+
+    //  Retira del almacén un contenedor particular y le asigna una parada dentro de una ruta existente
+    // La función recibe la pila que se seleccionó anteriormente y no devuelve el contenedor que se quiere sacar
+    // TODO: Modificar
+    public Contenedor retirarContenedor(PilaContenedores pilaActual) { // Eliminar contenedor de una pila ya elegida
+        Scanner sc = new Scanner(System.in);
+
+        if (pilaActual.isEmpty()) {
             System.out.println("No hay contenedores disponibles para retirar.");
         } else {
-            pilaActual.pop();
+            pilaActual
+            System.out.println("Escribe el contenedor que quieres retirar.");
         }
     }
 
-    public Object contenedorTope(PilaContenedores pilaActual) { // Mostrar el tope de la pila ya elegida
+    // La función recibe la pila que está siendo modificada y devuelve el contenedor que está al tope
+    public Object contenedorTope(PilaContenedores pilaActual) {
         return pilaActual.top();
     }
 
-    public boolean validarPila(PilaContenedores pilaActual) { // Revisar si la pila ya llegó al límite: 5
+    // Función que valida si la pila seleccionada (entrada) ya ha llegado a su límite y hay que crear otra (5 contenedores)
+    public boolean validarPila(PilaContenedores pilaActual) {
         if (pilaActual.size() > 4) {
             return false;
         }
--- src/pilas/PilaContenedores.java
+++ src/pilas/PilaContenedores.java
@@ -1,7 +1,6 @@
 package pilas;
 
 import listas.ListaSimple;
-import listas.Nodo;
 
 public class PilaContenedores implements Stack {
     protected ListaSimple lst;
